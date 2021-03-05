d94ee5a16818a3489f0434ae50e40a4a8c15909b
soham-bagchi-ug22 <72063456+soham-bagchi-ug22@users.noreply.github.com>
2021-03-06 00:34:33 +0530

    question3-a

---
+++ question3-a/__init__.py
@@ -0,0 +1,12 @@
+import check50
+import check50.c
+
+@check50.check()
+def compiles():
+  """question3-a compiles"""
+  check50.c.compile("question3-a.c")
+  
+@check50.check(compiles)
+def question3a():
+  """question3-a runs"""
+  check50.run("./question3-a").stdout("chocolate\nmelody\nsprite").exit(0)

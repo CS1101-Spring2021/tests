6ca2cbdf9a6b3c0ae3955604340ccf322d882473
soham-bagchi-ug22 <72063456+soham-bagchi-ug22@users.noreply.github.com>
2021-03-06 00:18:27 +0530

    check50 question2-b

---
+++ question2-b/__init__.py
@@ -0,0 +1,13 @@
+
+import check50
+import check50.c
+
+@check50.check()
+def compiles():
+  """question2-b compiles"""
+  check50.c.compile("question2-b.c")
+  
+@check50.check(compiles)
+def question2b():
+  """question2-b runs"""
+  check50.run("./question2-b").stdin("54 42", prompt=False).stdout("3\n6\n").exit(0)

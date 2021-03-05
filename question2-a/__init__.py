fd1f0d5a9b7bb95ba649cf95dce2a6ec8f862626
soham-bagchi-ug22 <72063456+soham-bagchi-ug22@users.noreply.github.com>
2021-03-05 23:55:39 +0530

    Update __init__.py

--- question2-a/__init__.py
+++ question2-a/__init__.py
@@ -6,7 +6,7 @@ def compiles()
   """question2-a compiles"""
   check50.c.compile("question2-a.c")
   
-@check50.check()
+@check50.check(compiles)
 def question2a()
   """question2-a runs"""
   check50.run("./question2-a").stdin("15 5").stdout("5\n").exit(0)

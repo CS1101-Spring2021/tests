c79757fd71e90f2f56f0b91d983a6b99a754809b
soham-bagchi-ug22 <72063456+soham-bagchi-ug22@users.noreply.github.com>
2021-03-15 12:53:06 +0530

    Create __init__.py

---
+++ mock/__init__.py
@@ -0,0 +1,17 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+  """question3-b exists"""
+  check50.exists("question3-b.c")
+
+@check50.check(exists)
+def compiles():
+  """question3-b compiles"""
+  check50.c.compile("question3-b.c")
+
+@check50.check(compiles)
+def question3a():
+  """question3-b runs"""
+  check50.run("./question3-b").stdin("3\nsix seven eight", prompt=False).stdout("eight\nseven\nsix\n").exit(0)

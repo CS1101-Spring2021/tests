2d49b9e4b7d3d4b3cd3eee4b4c791e683fe2dc9d
Soham Bagchi <soham.bagchi_ug22@ashoka.edu.in>
2021-04-12 15:19:34 +0530

    fixed q2 pigeon

--- question2/__init__.py
+++ question2/__init__.py
@@ -20,5 +20,5 @@ def question2():
     check50.run("./question2").stdin("13").stdin("5").stdout("1").stdin("1\napple").stdin(
         "1\norange").stdin("6").stdout("orange\napple").stdin("2").stdout("orange").stdin(
             "3").stdout("apple").stdin("2").stdout("apple").stdin("3").stdout("-1").stdin(
-                "1\nsparrow").stdout("1\npigeon").stdin("4").stdout("0").stdin("1\ncrow").stdin(
+                "1\nsparrow").stdin("1\npigeon").stdin("4").stdout("0").stdin("1\ncrow").stdin(
                     "3").stdout("crow").stdin("6").stdout("crow\npigeon\nsparrow").exit(0)

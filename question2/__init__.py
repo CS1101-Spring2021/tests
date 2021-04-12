3b310d584c264160c98f756a0c2e786baa1937de
Soham Bagchi <soham.bagchi_ug22@ashoka.edu.in>
2021-04-12 15:36:17 +0530

    fixed q2 count

--- question2/__init__.py
+++ question2/__init__.py
@@ -17,7 +17,7 @@ def compiles():
 @check50.check(compiles)
 def question2():
     """question2 runs"""
-    check50.run("./question2").stdin("13").stdin("5").stdout("1").stdin("1\napple").stdin(
+    check50.run("./question2").stdin("14").stdin("5").stdout("1").stdin("1\napple").stdin(
         "1\norange").stdin("6").stdout("orange\napple").stdin("2").stdout("orange").stdin(
             "3").stdout("apple").stdin("2").stdout("apple").stdin("3").stdout("-1").stdin(
                 "1\nsparrow").stdin("1\npigeon").stdin("4").stdout("0").stdin("1\ncrow").stdin(
